30611db7fac9d855e1d30fa76efe9fce368daa83
Muhammad Arrezza Faiz Pratama <103143006+panda560@users.noreply.github.com>
2022-05-31 13:47:20 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 /*
-› Create By @RIY
-› Base Ori @DikaArdnt
+› Create By @FAIZGANZ
+› Base Ori @M.A.F.P
 › Kalau mau tambahin fitur di index.js
 › Edit text menu di file language › help.js
 */
@@ -22,27 +22,27 @@ global.APIKeys = {
 }
 
 //—————「 Setting Owner 」—————//
-global.owner = ['6281575886399','6281575886399','6281575886399'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
-global.premium = ['6281575886399'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
-global.ownernomer = '6281575886399' //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.owner = ['+62 812-7367-7810','+62 812-7367-7810','+62 812-7367-7810'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.premium = ['+62 812-7367-7810'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.ownernomer = '+62 812-7367-7810' //ubah jadi nomor mu, note tanda ' gausah di hapus!
 
 //—————「 Set Nama Own & Bot 」—————//
-global.ownername = 'RIY' //ubah jadi nama mu, note tanda ' gausah di hapus!
-global.botname = 'Chika-Md' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
-global.footer = 'RIY' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.ownername = 'FAIZ' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.botname = 'Pb-MD' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
+global.footer = 'FAIZGANZ' //ubah jadi nama mu, note tanda ' gausah di hapus!
 
 //—————「 Set Kebutuhan Button & Kontak 」—————//
 //ubah aja kalau ada yang gapunya gsh di hps anggap aja credits :), note tanda ' gausah di hapus!
 global.youtube = 'https://youtube.com/channel/UCtM-pDaaGVRe4BJ7w4qE4Bw'
-global.ig = 'https://instagram.com/riychdwayne'
-global.mygc = 'https://chat.whatsapp.com/CRY23lVKmXB7sCMhHmcwVw'
-global.myweb = 'https://s.id/RiyAPIs'
-global.email = 'riychdwaynee@gmail.com'
+global.ig = 'https://www.instagram.com/invites/contact/?i=18n050noxcaqr&utm_content=4fkjlpc'
+global.mygc = 'https://chat.whatsapp.com/LYKAIdkBeX97FK0jNsOMAx'
+global.myweb = ''
+global.email = 'muhammadfgentas@gmail.com'
 global.region = 'indonesia'
 
 //—————「 Set Wm 」—————//
-global.packname = '© Chika-Md' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
-global.author = 'RIY' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.packname = '© Pb-MD' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
+global.author = 'FAIZGANZ' //ubah jadi nama mu, note tanda ' gausah di hapus!
 
 //—————「 Set Nama Session 」—————//
 //gausah di apa² in!
